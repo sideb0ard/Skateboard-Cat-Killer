505cbeb1076c0a34f22053e4ea88bb147bfe9a25
Thor Sideburns <thor@dropcam.com>
2014-07-13 18:07:21 -0700

    Backuuuupsz

--- game.py
+++ game.py
@@ -6,18 +6,13 @@ import random
 
 #2
 pygame.init()
-width,height = 940, 480
+width,height = 1140, 480
 screen = pygame.display.set_mode((width,height))
 
 keys = [False, False, False, False]
 
 acc=[0,0]
 arrows=[]
-
-badtimer=100
-badtimer1=0
-badguys=[[980,150]]
-healthvalue=194
 
 pygame.mixer.init()
 
@@ -28,12 +23,17 @@ skaterollie = pygame.image.load("resources/images/skater_ollie.png")
 skaterimg = skater
 skaterpos = [10, height - skater.get_height()]
 
-grass = pygame.image.load("resources/images/grass.png")
-castle = pygame.image.load("resources/images/castle.png")
-arrow = pygame.image.load("resources/images/bullet.png")
+catrun = pygame.image.load("resources/images/cat_run.png")
+catsplat = pygame.image.load("resources/images/cat_splat.png")
+catguyimg1 = catrun
+catguyimg=catguyimg1
 
-badguyimg1 = pygame.image.load("resources/images/badguy.png")
-badguyimg=badguyimg1
+cattimer=100
+cattimer1=0
+catguys=[[980,height - catrun.get_height() ]]
+healthvalue=194
+
+grass = pygame.image.load("resources/images/grass.png")
 
 healthbar = pygame.image.load("resources/images/healthbar.png")
 health = pygame.image.load("resources/images/health.png")
@@ -42,14 +42,17 @@ gameover = pygame.image.load("resources/images/gameover.png")
 youwin = pygame.image.load("resources/images/youwin.png")
 
 # 3.1 - Load audio
-hit = pygame.mixer.Sound("resources/audio/explode.wav")
-enemy = pygame.mixer.Sound("resources/audio/enemy.wav")
-shoot = pygame.mixer.Sound("resources/audio/shoot.wav")
-hit.set_volume(0.05)
-enemy.set_volume(0.05)
-shoot.set_volume(0.05)
-pygame.mixer.music.load('resources/audio/moonlight.wav')
-#pygame.mixer.music.play(-1, 0.0)
+olliepop = pygame.mixer.Sound("resources/audio/olliepop.wav")
+olliepop.set_volume(0.75)
+ollieland = pygame.mixer.Sound("resources/audio/ollieland.wav")
+ollieland.set_volume(0.75)
+
+catwah = pygame.mixer.Sound("resources/audio/catsqueek.wav")
+catwah.set_volume(0.65)
+
+#pygame.mixer.music.load('resources/audio/bass.wav')
+pygame.mixer.music.load('resources/audio/hoodlike.wav')
+pygame.mixer.music.play(-1, 0.0)
 pygame.mixer.music.set_volume(0.25)
 
 #4
@@ -57,7 +60,7 @@ pygame.mixer.music.set_volume(0.25)
 running = 1
 exitcode = 0
 while running:
-    badtimer-=1
+    cattimer-=1
     #5
     screen.fill(0)
 
@@ -65,71 +68,47 @@ while running:
     for x in range(width/grass.get_width()+1):
         for y in range(height/grass.get_height()+1):
             screen.blit(grass,(x*100,y*100))
-    #screen.blit(castle,(0,30))
-    #screen.blit(castle,(0,135))
-    #screen.blit(castle,(0,240))
-    #screen.blit(castle,(0,345))
 
-    #screen.blit(skater, skaterpos)
-    # 6.1 - Set skater position and rotation
-    #position = pygame.mouse.get_pos()
-    #angle = math.atan2(position[1]-(skaterpos[1]+32),position[0]-(skaterpos[0]+26))
-    #skaterrot = pygame.transform.rotate(skater, 360-angle*57.29)
-    #skaterpos1 = (skaterpos[0]-skaterrot.get_rect().width/2, skaterpos[1]-skaterrot.get_rect().height/2)
-    #screen.blit(skaterrot, skaterpos1)
-    # 6.2 - Draw arrows
     screen.blit(skaterimg, skaterpos)
-    for bullet in arrows:
-        index=0
-        velx=math.cos(bullet[0])*10
-        vely=math.sin(bullet[0])*10
-        bullet[1]+=velx
-        bullet[2]+=vely
-        if bullet[1]<-64 or bullet[1]>640 or bullet[2]<-64 or bullet[2]>480:
-            arrows.pop(index)
-        index+=1
-        for projectile in arrows:
-            arrow1 = pygame.transform.rotate(arrow, 360-projectile[0]*57.29)
-            screen.blit(arrow1, (projectile[1], projectile[2]))
-    # 6.3 - Draw badgers
-    if badtimer==0:
-        badguys.append([640, random.randint(50,430)])
-        badtimer=100-(badtimer1*2)
-        if badtimer1>=35:
-            badtimer1=35
-        else:
-            badtimer1+=5
+    # 6.3 - Draw cats
+    if cattimer==0:
+        catguys.append([980, height - catrun.get_height()])
+        cattimer=random.randint(20,200)
+        #cattimer=100-(cattimer1*2)
+        #if cattimer1>=35:
+        #    cattimer1=35
+        #else:
+        #    cattimer1+=5
     index=0
-#    for badguy in badguys:
-#        if badguy[0]<-64:
-#            badguys.pop(index)
-#        badguy[0]-=7
-#        # 6.3.1 - Attack castle
-#        badrect=pygame.Rect(badguyimg.get_rect())
-#        badrect.top=badguy[1]
-#        badrect.left=badguy[0]
-#        if badrect.left<64:
-#            # section 6.3.1 after if badrect.left<64:
-#            #hit.play()
-#            healthvalue -= random.randint(5,20)
-#            badguys.pop(index)
-#        #6.3.2 - Check for collisions
-#        index1=0
-#        for bullet in arrows:
-#            bullrect=pygame.Rect(arrow.get_rect())
-#            bullrect.left=bullet[1]
-#            bullrect.top=bullet[2]
-#            if badrect.colliderect(bullrect):
-#                # section 6.3.2 after if badrect.colliderect(bullrect):
-#                #enemy.play()
-#                acc[0]+=1
-#                badguys.pop(index)
-#                arrows.pop(index1)
-#            index1+=1
-#        # 6.3.3 - Next bad guy
-#        index+=1
-#    for badguy in badguys:
-#        screen.blit(badguyimg, badguy)
+    for catguy in catguys:
+        if catguy[0]< -140:
+            catguys.pop(index)
+        catguy[0]-=7
+        # 6.3.1 - Attack castle
+        catrect=pygame.Rect(0, 0, (catguyimg.get_width() - 180), (catguyimg.get_height() - 180))
+        catrect.top=catguy[1]
+        print "CatguyTOP %d" % (catguy[1], )
+        catrect.left=catguy[0]
+        print "CatguyLEFT %d" % (catguy[0], )
+        if catrect.left<64:
+            # section 6.3.1 after if catrect.left<64:
+            #hit.play()
+            healthvalue -= random.randint(5,20)
+            catguys.pop(index)
+        #6.3.2 - Check for collisions
+        index1=0
+        skaterect=pygame.Rect(skater.get_rect())
+        skaterect.left=skaterpos[0] - 80
+        skaterect.top=skaterpos[1] - 50
+        if catrect.colliderect(skaterect):
+           catwah.play()
+           acc[0]+=1
+           catguys.pop(index)
+           screen.blit(catsplat, catguy)
+           index1+=1
+        index+=1
+    for catguy in catguys:
+        screen.blit(catguyimg, catguy)
 
     # 6.4 - Draw clock
     font = pygame.font.Font(None, 24)
@@ -150,22 +129,22 @@ while running:
             pygame.quit()
             exit(0)
         if event.type == pygame.KEYDOWN:
-            if event.key==K_w:
+            if event.key==K_UP:
                 keys[0]=True
-            elif event.key==K_a:
+            elif event.key==K_LEFT:
                 keys[1]=True
-            elif event.key==K_s:
+            elif event.key==K_DOWN:
                 keys[2]=True
-            elif event.key==K_d:
+            elif event.key==K_RIGHT:
                 keys[3]=True
         if event.type == pygame.KEYUP:
-            if event.key==K_w:
+            if event.key==K_UP:
                 keys[0]=False
-            elif event.key==K_a:
+            elif event.key==K_LEFT:
                 keys[1]=False
-            elif event.key==K_s:
+            elif event.key==K_DOWN:
                 keys[2]=False
-            elif event.key==K_d:
+            elif event.key==K_RIGHT:
                 keys[3]=False
         #if event.type==pygame.MOUSEBUTTONDOWN:
         #    # section 8, after if event.type==pygame.MOUSEBUTTONDOWN:
@@ -174,22 +153,39 @@ while running:
         #    acc[1]+=1
         #    arrows.append([math.atan2(position[1]-(skaterpos1[1]+32),position[0]-(skaterpos1[0]+26)),skaterpos1[0]+32,skaterpos1[1]+32])
     #9
+    print "skaterpos[1] is %d and height minus skater height is %d" % (skaterpos[1], height - skater.get_height())
+    #if (skaterpos[1] == (height - skater.get_height())) and (skaterpos[0] + skater.get_width() < width) :
+    if skaterimg == skaterollie and (skaterpos[1] == height - skater.get_height()) :
+        ollieland.play()
+    if (skaterpos[1] == (height - skater.get_height())) : # and (skaterpos[0] + skater.get_width() < width) :
+        skaterimg = skater
+    if keys[0] and (skaterpos[1] == height - skater.get_height()) :
+        olliepop.play()
     if keys[0] and (skaterpos[1] > 1):
         skaterimg = skaterpop
-        skaterpos[0]+=5
-        skaterpos[1]-=5
+        #olliepop.play()
+        skaterpos[0]+=7
+        skaterpos[1]-=7
     elif keys[2] and (skaterpos[1] + skater.get_height() < height) :
         #print "skaterpos[1] is %d and skater height is %d" % (skaterpos[1], skater.get_height())
-        skaterpos[1]+=5
+        skaterpos[1]+=7
+    elif keys[1] and (skaterpos[0] > 0 ) and (skaterpos[1] + skater.get_height() < height ) and skaterimg == skaterollie: 
+        skaterpos[0]-=7
+        skaterpos[1]+=7
     elif keys[1] and (skaterpos[0] > 0 ):
         print "skaterpos[0] is %d and skater width is %d" % (skaterpos[0], skater.get_width())
-        skaterpos[0]-=5
+        skaterpos[0]-=7
     elif keys[3] and (skaterpos[0] + skater.get_width() < width) and (skaterimg == skaterpop) :
         skaterimg = skaterollie
-        skaterpos[0]+=5
-        skaterpos[1]+=5
+        skaterpos[0]+=7
+        skaterpos[1]+=7
     elif keys[3] and (skaterpos[0] + skater.get_width() < width) :
-        skaterpos[0]+=5
+        skaterpos[0]+=7
+    elif (skaterpos[1] < (height - skater.get_height())) :
+        skaterimg = skaterollie
+        skaterpos[0]+=7
+        skaterpos[1]+=7
+
         #10 - Win/Lose check
     if pygame.time.get_ticks()>=90000:
         running=0
